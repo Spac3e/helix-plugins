f990b48b41f738bfcc41aa12957499777f65908a
Artem <100233737+Spac3e@users.noreply.github.com>
2023-01-29 15:29:41 +0300

    Update sh_plugin.lua

--- ix_shieldbreaker/sh_plugin.lua
+++ ix_shieldbreaker/sh_plugin.lua
@@ -1,13 +1,3 @@
 PLUGIN.name = "Shield Breaker"
 PLUGIN.author = "-Spac3"
-PLUGIN.description = "Clockwork port to helix"
-
-ix.config.Add("forcefieldBrakeStrip", 5, "Get the forcefield braker after use", nil, {
-	data = {min = 0, max = 60},
-	category = PLUGIN.name
-})
-
-ix.config.Add("forcefieldBrakeTime", 5, "How long does it take to break the forcefield", nil, {
-	data = {min = 0, max = 60},
-	category = PLUGIN.name
-})
+PLUGIN.description = "Contains EMP device for hacking"
