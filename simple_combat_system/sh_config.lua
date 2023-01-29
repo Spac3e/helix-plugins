cd2518ba193214c01731a3b7e2123dc713cc2ac1
Artem <100233737+Spac3e@users.noreply.github.com>
2023-01-29 15:35:01 +0300

    New verison of Simple Combat System

---
+++ simple_combat_system/sh_config.lua
@@ -0,0 +1,41 @@
+WEAPON_STATS = WEAPON_STATS or {} -- add you're weapons here
+WEAPON_STATS["tfa_rtbr_smg1"] = {
+	["damage"] = 15,
+	["attrib"] = 10,
+	["range"] = 40
+}
+WEAPON_STATS["tfa_rtbr_pistol"] = {
+	["damage"] = 11,
+	["attrib"] = 50,
+	["range"] = 30
+}
+WEAPON_STATS["tfa_rtbr_akm"] = {
+	["damage"] = 20,
+	["attrib"] = 15,
+	["range"] = 50
+}
+WEAPON_STATS["tfa_rtbr_oicw"] = {
+	["damage"] = 19,
+	["attrib"] = 100,
+	["range"] = 1
+}
+WEAPON_STATS["tfa_rtbr_shotgun"] = {
+	["damage"] = 10,
+	["attrib"] = 10,
+	["range"] = 15
+}
+WEAPON_STATS["tfa_rtbr_357"] = {
+	["damage"] = 25,
+	["attrib"] = 10,
+	["range"] = 80
+}
+WEAPON_STATS["tfa_rtbr_sniperrifle"] = {
+	["damage"] = 60,
+	["attrib"] = 15,
+	["range"] = 100
+}
+WEAPON_STATS["tfa_rtbr_alyxgun"] = {
+	["damage"] = 10,
+	["attrib"] = 10,
+	["range"] = 15
+}
